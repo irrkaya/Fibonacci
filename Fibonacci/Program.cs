9a9ad810bec3fc82ce2838aea0f75026f4fa16b1
Irem Soyaker <irem_95@windowslive.com>
2022-07-11 19:31:26 +1000

    Add project files.

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var fibonacciNumbers = new List<int> { 1, 1 };
+            
+            while (fibonacciNumbers.Count < 20)
+            {
+                var firstFibonacci = fibonacciNumbers[fibonacciNumbers.Count - 1];
+                var secondFibonacci = fibonacciNumbers[fibonacciNumbers.Count - 2];
+                fibonacciNumbers.Add(firstFibonacci + secondFibonacci);
+               
+            }
+            foreach (var fibonacci in fibonacciNumbers)
+            {
+                Console.WriteLine(fibonacci);
+        
+            }
+        }
+    }
+}
